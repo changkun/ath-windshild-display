f9a8a99d8596a507df18a684c47923170a353d3c
Changkun Ou <hi@changkun.us>
2017-07-02 13:00:58 +0200

    (add): first works, need environment for tunes

--- kinect/main.py
+++ kinect/main.py
@@ -1,110 +1,65 @@
-# # import the necessary modules
-# import freenect
-# import cv2
-# import numpy as np
-
-# # function to get RGB image from kinect
-
-
-# def get_video():
-#     array, _ = freenect.sync_get_video()
-#     #array = cv2.cvtColor(array, cv2.COLOR_RGB2BGR)
-#     return array
-
-# # function to get depth image from kinect
-
-
-# def get_depth():
-#     array, _ = freenect.sync_get_depth()
-#     array = array.astype(np.uint8)
-#     return array
-
-
-# if __name__ == "__main__":
-#     while 1:
-#         # get a frame from RGB camera
-#         frame = get_video()
-#         # get a frame from depth sensor
-#         depth = get_depth()
-#         # display RGB image
-#         cv2.imshow('RGB image', frame)
-#         # display depth image
-#         cv2.imshow('Depth image', depth)
-
-#         # quit program when 'esc' key is pressed
-#         k = cv2.waitKey(5) & 0xFF
-#         if k == 27:
-#             break
-#     cv2.destroyAllWindows()
-
-import pygame
+# import the necessary modules
+import freenect
+import cv2
 import numpy as np
-import sys
-from freenect import sync_get_depth as get_depth
-
-# Check here for details
-# https://github.com/OpenKinect/libfreenect/blob/master/examples/glview.c#L350-L402
+import time
+import keystroke
 
 
-def make_gamma():
-    """
-    Create a gamma table
-    """
-    num_pix = 2048  # there's 2048 different possible depth values
-    npf = float(num_pix)
-    _gamma = np.empty((num_pix, 3), dtype=np.uint16)
-    for i in xrange(num_pix):
-        v = i / npf
-        v = pow(v, 3) * 6
-        pval = int(v * 6 * 256)
-        lb = pval & 0xff
-        pval >>= 8
-        if pval == 0:
-            a = np.array([255, 255 - lb, 255 - lb], dtype=np.uint8)
-        elif pval == 1:
-            a = np.array([255, lb, 0], dtype=np.uint8)
-        elif pval == 2:
-            a = np.array([255 - lb, lb, 0], dtype=np.uint8)
-        elif pval == 3:
-            a = np.array([255 - lb, 255, 0], dtype=np.uint8)
-        elif pval == 4:
-            a = np.array([0, 255 - lb, 255], dtype=np.uint8)
-        elif pval == 5:
-            a = np.array([0, 0, 255 - lb], dtype=np.uint8)
-        else:
-            a = np.array([0, 0, 0], dtype=np.uint8)
-
-        _gamma[i] = a
-    return _gamma
-
-
-gamma = make_gamma()
+def get_depth():
+    array, _ = freenect.sync_get_depth()
+    array = array.astype(np.uint8)
+    return array
 
 
 if __name__ == "__main__":
-    fpsClock = pygame.time.Clock()
-    FPS = 30  # kinect only outputs 30 fps
-    disp_size = (640, 480)
-    pygame.init()
-    screen = pygame.display.set_mode(disp_size)
-    font = pygame.font.Font(pygame.font.match_font(
-        'Courier'), 32)  # provide your own font
-    while True:
-        events = pygame.event.get()
-        for e in events:
-            if e.type == pygame.QUIT:
-                sys.exit()
-        fps_text = "FPS: {0:.2f}".format(fpsClock.get_fps())
-        # draw the pixels
+    # calculate initial environment brightness
+    step = 10
+    init_bright = 0
+    depth = get_depth()
+    time.sleep(5)      # five sec for preparation
+    for x in range(0, depth.shape[0], step):
+        for y in range(0, depth.shape[1], step):
+            init_bright += depth[x][y] / 255.0
 
-        # get the depth readinngs from the camera
-        depth = np.rot90(get_depth()[0])
-        # the colour pixels are the depth readings overlayed onto the gamma
-        # table
-        pixels = gamma[depth]
-        temp_surface = pygame.Surface(disp_size)
-        pygame.surfarray.blit_array(temp_surface, pixels)
-        pygame.transform.scale(temp_surface, disp_size, screen)
-        screen.blit(font.render(fps_text, 1, (255, 255, 255)), (30, 30))
-        pygame.display.flip()
-        fpsClock.tick(FPS)
+    print('init_bright: ', init_bright)
+
+    last_level = 0
+    while True:
+        global_bright = 0
+        depth = get_depth()
+        brightness_img = np.zeros(depth.shape, np.uint8)
+        bright_points_num = (depth.shape[0] / step) * (depth.shape[1] / step)
+
+        for x in range(0, depth.shape[0], step):
+            for y in range(0, depth.shape[1], step):
+                bright = depth[x][y]
+                global_bright += bright / 255.0
+                cv2.rectangle(brightness_img, (y, x),
+                              (y + step / 2, x + step / 2), float(bright), -1)
+        global_bright -= init_bright
+        # print('global_bright:', global_bright)
+        cv2.putText(brightness_img, 'global_bright: ' + str(global_bright),
+                    (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0))
+
+        cv2.imshow('brightness image', brightness_img)
+
+        current_level = int(global_bright / 100)
+        print(current_level, last_level, current_level - last_level)
+
+        if current_level - last_level == 1:
+            keystroke.press_keys('increase')
+            print('increase')
+        elif current_level - last_level == -1:
+            keystroke.press_keys('decrease')
+            print('decrease')
+        else:
+            print('satble')
+
+        last_level = current_level
+
+        k = cv2.waitKey(5) & 0xFF
+        if k == 27:
+            break
+
+    cv2.destroyAllWindows()
